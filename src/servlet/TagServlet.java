d32f35121f450cc7781f8d05e1fdfaea86fd227f
Trowy <growety@gmail.com>
2014-04-24 10:30:16 +0700

    add tags error

--- src/servlet/TagServlet.java
+++ src/servlet/TagServlet.java
@@ -90,11 +90,17 @@ public class TagServlet extends HttpServlet {
 				s.removeTag(Integer.parseInt(request.getParameter("id")));
 				
 		}
-		
-		} catch (CRMException e1) {			
-			e1.printStackTrace();
-		}
 		response.getWriter().print("{success: true}");
+		
+		
+		} catch (CRMException e1) {
+			String field = "name";
+			if(e1.field_num==1){
+				field = "name";
+			}
+			response.getWriter().print("{success:false, errors: {"+field+":'"+e1.getMessage().replace("'", "\\'")+"'}}");
+			
+		}
 		response.getWriter().flush();
         response.getWriter().close();
         }
